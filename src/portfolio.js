4f90f7a6b1a06e79da08bbf909cafcca221fbf90
DESKTOP-GPOD68E\Tofail <tofailhiary@hotmail.com>
2023-12-03 21:47:26 +0300

    add colors for skills , fix some spaces issue

--- src/portfolio.js
+++ src/portfolio.js
@@ -21,9 +21,9 @@ const illustration = {
 
 const greeting = {
   username: "Al-Tofail Al-Hiary",
-  title: "Hi all, Al-Tofail",
+  title: "Hi all, I'm Al-Tofail",
   subTitle: emoji(
-    "A dedicated Senior Quality Assurance Engineer (ISTQB® CTFL/CTAL-TA/CTAL-TTA) 🚀 with extensive experience in ensuring the quality and reliability of both Web and Mobile applications. Proficient in a range of testing methodologies and tools, including Selenium and Appium, and adept in handling complex quality assurance challenges in diverse environments."
+    "A dedicated Senior Quality Assurance Engineer (ISTQB® CTFL/CTAL-TA/CTAL-TTA) 🚀 with extensive experience in ensuring the quality and reliability of both Web and Mobile applications. Proficient in a range of testing methodologies and tools, including Selenium , Appium and Postman, and adept in handling complex quality assurance challenges in diverse environments."
   ),
   resumeLink:
     "https://docs.google.com/document/d/1lg0n7kbTfwvkXEXo-rElKU6B_dAZCC4T/edit?usp=sharing&ouid=112975303881704365343&rtpof=true&sd=true", // Set to empty to hide the button
@@ -207,46 +207,46 @@ const workExperiences = {
   display: true, //Set it to true to show workExperiences Section
   experience: [
     {
-      role: "Software Test Engineer",
+      role: "Software Test Engineer/Quality Control Engineer",
       company: "ETQ",
       companylogo: require("./assets/images/Etq.png"),
       date: "Mar 1,2016 – Jun 11,2018",
-      desc: "In summary, this role encompasses the entire software testing lifecycle: understanding requirements, preparing test plans and cases, managing test environments, executing manual and automated tests, and defect reporting. It involves continuous updates of test cases and automation scripts, effective communication with stakeholders, and staying current with testing tools and industry trends. The role requires a deep understanding of software development, proactive quality assurance, and team collaboration.",
+      desc: "This role covers all aspects of software testing: comprehending requirements, creating test plans and cases, handling test environments, conducting both manual and automated tests, and reporting defects. It includes updating test cases and automation scripts, communicating with stakeholders, and keeping up-to-date with testing tools and industry trends. A thorough understanding of software development, proactive quality assurance, and teamwork are essential.",
     },
     {
-      role: "Quality Control Engineer/ Quality Automation Engineer",
+      role: "Quality Control Engineer/Quality Automation Engineer",
       company: "Baaz",
       companylogo: require("./assets/images/Baaz.png"),
       date: "Jun 24,2018  – Jun 4,2019",
-      desc: "This role encompasses developing and executing comprehensive software testing strategies, including creating and automating test cases using Appium, Java, and JBehave, and managing release testing. Responsibilities include manual API testing, reviewing specifications, planning and monitoring test cycles, and utilizing Zephyr for test case creation. The role involves mobile testing across platforms, supervising testing activities, maintaining logs, reporting via JIRA, and presenting to stakeholders. Key aspects include staying updated with software, generating test cases based on requirements, reporting defects, and continuously refining test cases and automation processes for quality assurance and team efficiency."
+      desc: "This role involves developing and implementing software testing strategies, automating test cases with Appium, Java, and JBehave, and overseeing release testing. Key responsibilities include manual API testing, analyzing specifications, managing test cycles, and using Zephyr for test case development. It requires mobile testing across different platforms, supervising testing processes, maintaining logs, reporting in JIRA, and communicating with stakeholders. The focus is on keeping up-to-date with software trends, creating test cases from requirements, defect reporting, and continuously improving test cases and automation for better quality assurance and team efficiency."
     },
     {
-      role: "Quality Control Engineer/ Quality Automation Engineer",
+      role: "Quality Control Engineer/Quality Automation Engineer",
       company: "Shepherd",
       companylogo: require("./assets/images/Shepherd.jpg"),
       date: "Jun 10,2019 – Feb 14,2020",
-      desc: "This role entails creating Selenium and Cucumber test scripts in Java, setting up Jenkins for continuous testing, and conducting manual API testing. It involves reviewing specifications, planning sprint test cycles, monitoring service logs, and reporting results with TFS. Responsibilities include supervising testing activities, developing test plans, executing tests, and updating test cases and automation based on findings. The role requires a thorough understanding of software requirements and contributing to the improvement of testing processes."
+      desc: "This role focuses on developing Selenium and Cucumber test scripts in Java, implementing Jenkins for continuous testing, and performing manual API testing. Tasks include reviewing specifications, organizing sprint test cycles, tracking service logs, and reporting via TFS. Key responsibilities are overseeing testing activities, creating test plans, executing tests, and refining test cases and automation. A deep understanding of software requirements and enhancing testing processes is essential."
     },
     {
-      role: "Quality Control Engineer/ Quality Automation Engineer",
+      role: "Quality Control Engineer/Quality Automation Engineer",
       company: "Mawdoo3.com",
       companylogo: require("./assets/images/Mawdoo3.jpg"),
       date: "Feb 16,2020 – May 20,2021",
-      desc: "This role focuses on developing and enhancing testing frameworks, including creating Selenium test cases with Java, enhancing frameworks with utilities like Extent Report, and automating scripts with Cucumber. It involves setting up continuous builds for automated testing using Jenkins, conducting manual API tests, reviewing specifications, and planning test cycles for sprints. Key tasks also include monitoring service logs, reporting with Jira, and supervising testing activities including plan development and progress tracking. The role requires understanding software requirements, becoming familiar with the software under test, and generating test cases from requirements. Responsibilities extend to executing tests, reporting defects, updating test cases and automations based on findings, and contributing to process improvements within the team."
+      desc: "This role entails developing and improving testing frameworks by creating Selenium test cases in Java, integrating utilities like Extent Report, and automating scripts with Cucumber. It includes setting up Jenkins for automated test builds, performing manual API testing, examining specifications, and organizing sprint test cycles. Key responsibilities are monitoring service logs, using Jira for reporting, overseeing testing activities including planning and tracking. Understanding software requirements, familiarizing with the tested software, and creating test cases based on these requirements are crucial. The role also involves executing tests, defect reporting, updating test cases and automation as per findings, and aiding in team process enhancements."
     },
     {
-      role: "Quality Automation Engineer",
+      role: "Quality Automation Engineer/Quality Control Engineer",
       company: "MAJID ALFUTTAIM",
       companylogo: require("./assets/images/MAJID ALFUTTAIM.jpg"),
       date: "May 20,2021 – Mar 17,2022",
-      desc: "This role combines creating Selenium and Cucumber automation scripts in Java, setting up Jenkins for continuous testing, and performing manual API testing. It involves reviewing project specifications, planning test cycles for sprints, monitoring system logs, and using JIRA for reporting test outcomes. The position includes supervisory duties like developing test plans, tracking progress, and executing both manual and automated tests. Additionally, it encompasses understanding software requirements, generating test cases, updating test scripts based on defects, and contributing to the improvement of testing processes, with a key responsibility for the quality assurance of a specific Majid Al Futtaim product."
+      desc: "This role involves creating Selenium and Cucumber automation scripts in Java, implementing Jenkins for continuous testing, and conducting manual API tests. Responsibilities include reviewing project specs, organizing sprint test cycles, keeping track of system logs, and reporting via JIRA. The position requires supervisory tasks like developing test plans, overseeing progress, and executing tests. It also entails understanding software needs, creating test cases, updating scripts based on defects, and enhancing testing processes, with a focus on quality assurance for a specific Majid Al Futtaim product."
     },
     {
-      role: "lead Quality Control Engineer/ Quality Automation Engineer",
+      role: "lead Quality Control Engineer/Quality Automation Engineer",
       company: "Estarta",
       companylogo: require("./assets/images/estarta.png"),
       date: "Mar 2022 – Present",
-      desc: "This role is a comprehensive blend of technical and leadership responsibilities in software testing and quality assurance. It involves creating Selenium and Cucumber automation scripts in Java, implementing Jenkins for continuous testing, and performing manual API testing. The role requires reviewing specifications, planning test cycles, monitoring logs, and reporting using JIRA. As a Quality Team Lead, responsibilities extend to setting and overseeing quality processes for the team, leading quality-related activities, and conducting quality control training for over 15 resources. It also includes the development of an automation process and architecture, and working on an outsourced real estate project named ADRES. The position signifies progression to a leadership role, emphasizing both hands-on technical expertise and team management in quality assurance.."
+      desc: "This role combines technical expertise and leadership in software testing and quality assurance. Key responsibilities include creating Selenium and Cucumber automation scripts in Java, using Jenkins for continuous testing, and conducting manual API testing. The job involves reviewing specifications, managing test cycles, monitoring logs, and reporting with JIRA. As a Quality Team Lead, it encompasses overseeing quality processes, leading quality-related activities, and providing quality control training to over 15 team members. Additionally, the role includes developing automation processes and architecture, and contributing to an outsourced real estate project, ADRES. This position highlights a transition to a leadership role, balancing hands-on technical skills with team management in quality assurance."
     },
   ]
 };
@@ -298,13 +298,13 @@ const bigProjects = {
 const achievementSection = {
   title: emoji("Achievements And Certifications 🏆 "),
   subtitle:
-    "Achievements, Certifications, Award Letters and Some Cool Stuff that I have done !",
+    "Achievements, Certifications that I have done !",
 
   achievementsCards: [
     {
       title: "ISTQB - CTFL",
       subtitle:
-        "The Certified Tester Foundation Level (CTFL), offered by the International Software Testing Qualifications Board (ISTQB), is a certification aimed at professionals seeking a foundational understanding of software testing. It covers key concepts such as testing fundamentals, test design, test management, and tools, making it ideal for newcomers or those solidifying their knowledge in software testing. The CTFL is recognized globally and enhances professional credibility and career prospects in the field of software testing.",
+        "The Certified Tester Foundation Level (CTFL) from the International Software Testing Qualifications Board (ISTQB) is a certification for those seeking basic software testing knowledge. It covers fundamentals, test design, management, and tools, suitable for beginners or those reinforcing their skills. This globally recognized certification boosts professional credibility and career opportunities in software testing.",
       image: require("./assets/images/Ctfl.png"),
       imageAlt: "Google Code-In Logo",
       footerLink: [
@@ -317,7 +317,7 @@ const achievementSection = {
     {
       title: "ISTQB - CTAL-TA",
       subtitle:
-        "The Certified Tester Advanced Level - Test Analyst (CTAL-TA), provided by the International Software Testing Qualifications Board (ISTQB), is an advanced certification for professionals with foundational knowledge in software testing. It focuses on the role of a Test Analyst, emphasizing test analysis, design, and execution, along with an understanding of software development lifecycles. This certification is suitable for those looking to deepen their expertise in software testing and is recognized internationally, offering significant benefits for career advancement in the field.",
+        "The Certified Tester Advanced Level - Test Analyst (CTAL-TA) by ISTQB is an advanced certification for those with basic software testing knowledge. It concentrates on Test Analyst roles, covering test analysis, design, execution, and software development lifecycles. Ideal for professionals aiming to enhance their testing expertise, this internationally recognized certification aids in career advancement in the field.",
       image: require("./assets/images/CTAL-TA.png"),
       imageAlt: "Google Assistant Action Logo",
       footerLink: [
@@ -330,7 +330,7 @@ const achievementSection = {
 
     {
       title: "ISTQB - CTAL-TTA",
-      subtitle: "The Certified Tester Advanced Level - Technical Test Analyst (CTAL-TTA) is an advanced certification offered by the International Software Testing Qualifications Board (ISTQB). It is tailored for professionals who have already established a foundational understanding of software testing. This certification delves into more technical aspects of software testing, including topics like non-functional testing, white-box testing techniques, and test automation. The CTAL-TTA is designed for those looking to specialize in the technical dimensions of software testing, enhancing their skills in analyzing, designing, and executing tests in complex software environments. Holding this certification demonstrates a high level of expertise and is beneficial for career progression in specialized areas of software testing.",
+      subtitle: "The Certified Tester Advanced Level - Technical Test Analyst (CTAL-TTA) by ISTQB is an advanced certification for those with foundational software testing knowledge. It focuses on the technical aspects of testing, covering non-functional testing, white-box techniques, and test automation. Ideal for professionals seeking to specialize in technical software testing, it helps in developing skills for complex environments. This certification indicates expertise and supports career growth in specialized software testing areas.",
       image: require("./assets/images/CTAL-TTA.png"),
       imageAlt: "PWA Logo",
       footerLink: [
