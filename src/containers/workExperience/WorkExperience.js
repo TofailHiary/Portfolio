3b06331cc07db629b35e553ebcc87d49560df0d7
DESKTOP-GPOD68E\Tofail <tofailhiary@hotmail.com>
2023-12-08 15:56:01 +0300

    fix landscape and space experiances

--- src/containers/workExperience/WorkExperience.js
+++ src/containers/workExperience/WorkExperience.js
@@ -1,40 +1,64 @@
-import React, {useContext} from "react";
+import React, { useState, useEffect, useContext, useRef } from "react";
 import "./WorkExperience.scss";
 import ExperienceCard from "../../components/experienceCard/ExperienceCard";
-import {workExperiences} from "../../portfolio";
-import {Fade} from "react-reveal";
+import { workExperiences } from "../../portfolio";
+import { motion } from 'framer-motion';
 import StyleContext from "../../contexts/StyleContext";
 
 export default function WorkExperience() {
-  const {isDark} = useContext(StyleContext);
+  const { isDark } = useContext(StyleContext);
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          setIsVisible(true);
+          observer.disconnect();
+        }
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, []);
+
   if (workExperiences.display) {
     return (
-      <div id="experience">
-        <Fade bottom duration={1000} distance="20px">
-          <div className="experience-container" id="workExperience">
-            <div>
-              <h1 className="experience-heading">Experiences</h1>
-              <div className="experience-cards-div">
-                {workExperiences.experience.map((card, i) => {
-                  return (
-                    <ExperienceCard
-                      key={i}
-                      isDark={isDark}
-                      cardInfo={{
-                        company: card.company,
-                        desc: card.desc,
-                        date: card.date,
-                        companylogo: card.companylogo,
-                        role: card.role,
-                        descBullets: card.descBullets
-                      }}
-                    />
-                  );
-                })}
-              </div>
+      <div id="experience" ref={ref}>
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={isVisible ? { opacity: 1 } : {}}
+          transition={{ duration: 2.5 }}
+          className="experience-container" id="workExperience"
+        >
+          <div>
+            <h1 className="experience-heading">Experiences</h1>
+            <div className="experience-cards-div">
+              {workExperiences.experience.map((card, i) => (
+                <ExperienceCard
+                  key={i}
+                  isDark={isDark}
+                  cardInfo={{
+                    company: card.company,
+                    desc: card.desc,
+                    date: card.date,
+                    companylogo: card.companylogo,
+                    role: card.role,
+                    descBullets: card.descBullets,
+                  }}
+                />
+              ))}
             </div>
           </div>
-        </Fade>
+        </motion.div>
       </div>
     );
   }
